e12ecf4c43f92ef9ea225099b93ae608b187d26e
lwal0718 <66147155+lwal0718@users.noreply.github.com>
2020-07-13 08:50:09 -0400

    Create insta.py

---
+++ insta.py
@@ -0,0 +1,7 @@
+value = input("Please enter the username of the skid:\n")
+ 
+print(f'generating link for : {value}')
+
+value = input("username not found exploit shutting down try again y/n?:\n")
+
+print(f'user is not vuln at this time please try again at a later time')
